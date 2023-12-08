df6de73aaa99cfd6ba74132e328c48251ec75d16
Oshoaro Favour <oshoarofavour@gmail.com>
2023-12-08 09:58:01 +0100

    done with the skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import { HomeSection } from "./components/HomeSection";
 import { Navbar } from "./components/Navbar";
 import { Skills } from "./components/Skills";
@@ -5,13 +6,15 @@ import { SocialsSection } from "./components/SocialsSection";
 
 function App() {
   return (
-    <main className="bg-[#F0F8FF] text-[#000080] h-full w-full font-body">
-      <Navbar />
-      <HomeSection />
-      <SocialsSection />
-      <Skills />
-    </main>
-  )
+    <BrowserRouter>
+      <main className="bg-[#F0F8FF] text-[#000080] h-full w-full font-body">
+        <Navbar />
+        <HomeSection />
+        <SocialsSection />
+        <Skills />
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
-import Css from '../assets/css.png'
-import GitHub from '../assets/github.png'
-import Html from '../assets/html.png'
-import Javascript from '../assets/javascript.png'
-import MongoDB from '../assets/mongo.png'
-import NextJs from '../assets/nextjs.png'
-import Node from '../assets/node.png'
-import React from '../assets/react.png'
-import Tailwind from '../assets/tailwind.png'
+import React from "react";
+import Css from "../assets/css.png";
+import GitHub from "../assets/github.png";
+import Html from "../assets/html.png";
+import Javascript from "../assets/javascript.png";
+import MongoDB from "../assets/mongo.png";
+import NextJs from "../assets/nextjs.png";
+import NodeJs from "../assets/node.png";
+import ReactJs from "../assets/react.png";
+import Tailwind from "../assets/tailwind.png";
 
 export const Skills = () => {
   return (
-    <section>
-        <h2 className='text-lg font-semibold'>Technologies I currently work with:</h2>
-        <div>
-
+    <section className="border border-red-600 mt-24">
+      <div className="mt-3 ml-5">
+      <h1 className="font-bold text-2xl inline border-b-2 border-gray-800">Skills</h1>
+      <h2 className="text-lg mt-3">
+        Technologies I currently work with:
+      </h2>
+      </div>
+      <div className="grid grid-cols-3 gap-y-6 gap-x-10 py-8 mx-16">
+        <div className='skills-container'>
+          <img src={Html} alt="html-img" className="h-20"/>
+          <p className="skills-text">HTML</p>
         </div>
+        <div className='skills-container'>
+          <img src={Css} alt="css-img" className="h-20"/>
+          <p className="skills-text">CSS</p>
+        </div>
+        <div className='skills-container'>
+          <img src={Javascript} alt="js-img" className="h-20"/>
+          <p className="skills-text">JAVASCRIPT</p>
+        </div>
+        <div className='skills-container'>
+          <img src={GitHub} alt="github-img" className="h-20"/>
+          <p className="skills-text">GITHUB</p>
+        </div>
+        <div className='skills-container'>
+          <img src={Tailwind} alt="tailwind-img" className="h-20"/>
+          <p className="skills-text">TAILWIND CSS</p>
+        </div>
+        <div className='skills-container'>
+          <img src={NodeJs} alt="nodejs-img" className="h-20"/>
+          <p className="skills-text">NODE JS</p>
+        </div>
+        <div className='skills-container'>
+          <img src={NextJs} alt="nextjs-img" className="h-20"/>
+          <p className="skills-text">NEXT JS</p>
+        </div>
+        <div className='skills-container'>
+          <img src={MongoDB} alt="mongodb-img" className="h-20"/>
+          <p className="skills-text">MONGODB</p>
+        </div>
+        <div className='skills-container'>
+          <img src={ReactJs} alt="reactjs-img" className="h-20 animate-spin"/>
+          <p className="skills-text">REACT JS</p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
