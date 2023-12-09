25f22d14e6ffbf9ce91e5210282b7ddc7904c2a0
Oshoaro Favour <oshoarofavour@gmail.com>
2023-12-09 12:59:51 +0100

    made some changes

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -4,7 +4,7 @@ import { FaArrowAltCircleUp } from "react-icons/fa";
 
 export const ScrollToTop = () => {
   return (
-    <div className="fixed bottom-28 right-12">
+    <div className="mt-16 fixed bottom-28 right-5">
       <FaArrowAltCircleUp className=" text-[#3366CC] text-4xl animate-bounce" />
     </div>
   );
