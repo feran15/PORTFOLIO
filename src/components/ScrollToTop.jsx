7d820f771c8e75835d3632154f341cc55eb85e5a
Oshoaro Favour <oshoarofavour@gmail.com>
2023-12-11 19:25:34 +0100

    made some changes

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -4,7 +4,7 @@ import { HashLink as Link } from "react-router-hash-link";
 
 export const ScrollToTop = () => {
   return (
-    <div className="mt-16 fixed bottom-28 right-5">
+    <div className="mt-16 fixed bottom-16 right-5">
       <Link smooth to="#top">
         <FaArrowAltCircleUp className=" text-[#3366CC] text-2xl md:text-4xl animate-bounce" />
       </Link>
