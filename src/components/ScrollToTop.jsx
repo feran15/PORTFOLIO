7cb6b5fc382a85e1e632f5df14e4120f2470bec0
Oshoaro Favour <oshoarofavour@gmail.com>
2023-12-11 14:25:32 +0100

    worked on the responsiveness

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -6,7 +6,7 @@ export const ScrollToTop = () => {
   return (
     <div className="mt-16 fixed bottom-28 right-5">
       <Link smooth to="#top">
-        <FaArrowAltCircleUp className=" text-[#3366CC] text-4xl animate-bounce" />
+        <FaArrowAltCircleUp className=" text-[#3366CC] text-2xl md:text-4xl animate-bounce" />
       </Link>
     </div>
   );
