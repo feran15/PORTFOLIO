2e6ae33bca141d6a86eae0e904fd428ba19ca308
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-10 19:16:44 +0100

    added a new project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,6 +6,7 @@ import Quizapp from "../assets/quizapp.png";
 import Weatherapp from "../assets/weatherapp.png";
 import TaskManagementApp from "../assets/tma.png";
 import Musicapp from "../assets/musicapp.png";
+import AccessoryOasis from "../assets/accessoryoasis.png";
 import { FaGithub } from "react-icons/fa6";
 import { FaLink } from "react-icons/fa6";
 import Aos from "aos";
@@ -34,12 +35,47 @@ export const Works = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 md:gap-x-14 gap-y-10 mx-14 md:mx-16 py-10 text-[#0C0C0C]">
           <div className="works-container " data-aos="fade-up">
             <img
+              src={AccessoryOasis}
+              alt="foodspace-img"
+              className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
+            />
+            <div>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                ACCESSORY-OASIS
+              </h1>
+              <p className="mb-3 px-2 text-sm md:text-base">
+                A Full-satck e-commerce web application offering a variety of accessories where
+                users can explore different categories each displaying products
+                with their corresponding prices. Users have the flexibility to
+                easily add items from any category to their shopping cart and
+                make a payment.
+              </p>
+              <span className="flex flex-wrap gap-2 px-2">
+                <p className="works-pill">NextJs</p>
+                <p className="works-pill">TailwindCSS</p>
+                <p className="works-pill">ExpressJs</p>
+                <p className="works-pill">MongoDB</p>
+              </span>
+            </div>
+            <span className="mt-3 mx-2 pt-2 border-t border-[#0C0C0C] flex items-center justify-between">
+              <Link to="https://github.com/favourAyomikun/AccessoryOasis-client">
+                <FaGithub className="text-lg" />
+              </Link>
+              <Link to="https://accessory-oasis.vercel.app/">
+                <FaLink className="text-lg" />
+              </Link>
+            </span>
+          </div>
+          <div className="works-container " data-aos="fade-up">
+            <img
               src={Foodspace}
               alt="foodspace-img"
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">FOOD-SPACE</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                FOOD-SPACE
+              </h1>
               <p className="mb-3 px-2 text-sm md:text-base">
                 An e-commerce website offering a variety of foods, drinks, and
                 snacks, where users can explore different categories, each
@@ -68,7 +104,9 @@ export const Works = () => {
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">MUSIC APP</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                MUSIC APP
+              </h1>
               <p className="text-gray-800 mb-3 px-2 text-sm md:text-base">
                 This music web application allows users to play default songs,
                 add them to playlists, and discover the artists behind each
@@ -98,7 +136,9 @@ export const Works = () => {
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">FAN</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                FAN
+              </h1>
               <p className="text-gray-800 mb-3 px-2 text-sm md:text-base">
                 This is a fan designed with the ability to rotate.
               </p>
@@ -124,7 +164,9 @@ export const Works = () => {
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">BLOG APP</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                BLOG APP
+              </h1>
               <p className="text-gray-800 mb-3 px-2 text-sm md:text-base">
                 This blog web application enables users to create accounts, log
                 in, generate and publish content, as well as edit and view
@@ -153,7 +195,9 @@ export const Works = () => {
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">QUIZ APP</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                QUIZ APP
+              </h1>
               <p className="text-gray-800 mb-3 px-2 text-sm md:text-base">
                 A quiz web application designed to challenge users with
                 thought-provoking questions. Users can test their knowledge,
@@ -180,7 +224,9 @@ export const Works = () => {
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">WEATHER APP</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                WEATHER APP
+              </h1>
               <p className="text-gray-800 mb-3 px-2 text-sm md:text-base">
                 This is a web app that allows users to check the current weather
                 conditions of a city or state.
@@ -207,9 +253,12 @@ export const Works = () => {
               className="mb-4 rounded-md rounded-b-none w-full h-52 object-center object-cover"
             />
             <div>
-              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">TASK APP</h1>
+              <h1 className="text-base md:text-lg font-semibold mb-1 px-2">
+                TASK APP
+              </h1>
               <p className="text-gray-800 mb-3 px-2 text-sm md:text-base">
-                This is a web app which allows users to create, edit and delete tasks.
+                This is a web app which allows users to create, edit and delete
+                tasks.
               </p>
               <span className="flex flex-wrap gap-2 px-2">
                 <p className="works-pill">React.Js</p>
